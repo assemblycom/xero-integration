7545a691b62424545423edb070094e634bbdac7b
SandipBajracharya <sndp106.sb@gmail.com>
2026-03-10 18:54:24 +0545

    hotfix: update max duration for retry failed syncs route

--- src/app/cron/retry-failed-syncs/route.ts
+++ src/app/cron/retry-failed-syncs/route.ts
@@ -1,4 +1,6 @@
 import { retryFailedSyncs } from '@failed-syncs/api/failedSyncs.controller'
 import { withErrorHandler } from '@/utils/withErrorHandler'
 
+export const maxDuration = 300
+
 export const GET = withErrorHandler(retryFailedSyncs)
